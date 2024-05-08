975dc55b1c54a7ec0e0d39b5b793f667f02ea65d
Mikayil <mihaelhasanov@gmail.com>
2024-05-08 14:01:27 +0200

    first commit

---
+++ src/main/java/com/example/combinedatasets/CombineDatasetsApplication.java
@@ -0,0 +1,13 @@
+package com.example.combinedatasets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombineDatasetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CombineDatasetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/combinedatasets/CombineDatasetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.combinedatasets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CombineDatasetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
