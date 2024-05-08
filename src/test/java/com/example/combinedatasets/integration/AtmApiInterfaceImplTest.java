6ca29edb5cace7dc4f1485e8dd295e15f5b5bbd1
Mikayil <mihaelhasanov@gmail.com>
2024-05-08 15:29:43 +0200

    Established_connectionToCS_PlacesApi
    added apikey and url to properties file, tested/displayed response of the CsPlaces api

---
+++ src/test/java/com/example/combinedatasets/integration/AtmApiInterfaceImplTest.java
@@ -0,0 +1,21 @@
+package com.example.combinedatasets.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AtmApiInterfaceImplTest {
+
+    @Autowired
+    private AtmApiInterfaceImpl atmApiInterface;
+
+    @Test
+    void callSporitelnaAtmsList() {
+        ResponseEntity<String> response = atmApiInterface.callSporitelnaAtmsList();
+        System.out.println("API Response: " + response.getBody());
+    }
+}
